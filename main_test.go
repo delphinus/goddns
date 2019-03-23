895a77d407094e103ef184d5864cdf27ca1cefaa
delphinus <me@delphinus.dev>
2019-03-23 17:23:35 +0900

    Use testing.M to initialize tests

--- main_test.go
+++ main_test.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
+	"testing"
 
 	"github.com/google/logger"
 )
 
-func init() {
+func TestMain(m *testing.M) {
 	_ = logger.Init("goddns", true, false, ioutil.Discard)
+	os.Exit(m.Run())
 }
